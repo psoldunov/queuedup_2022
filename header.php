9c6914bf610920a4b219220656317bd61e5f5b08
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2022-09-12 18:28:27 +0300

    beta fixes

--- header.php
+++ header.php
@@ -1,3 +1,4 @@
+<?php global $wp; ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,10 +8,29 @@
     <?php
       echo get_bloginfo('name');
       wp_title();
-      ?>
+      ?> | Identify creators in the gaming space who are on the rise, and give them a launch pad to stardom.
   </title>
-  <link href="<?php echo get_template_directory_uri(); ?>/images/favicon.png" rel="shortcut icon" type="image/x-icon" />
-  <link href="<?php echo get_template_directory_uri(); ?>/images/webclip.png" rel="apple-touch-icon" />
+  <meta name="description"
+    content="The selected creators will receive benefits and prize packages designed to help scale their brands, including a 12-month paid contract with HyperX and an all-expense paid trip to Los Angeles, CA.">
+  <meta property="og:title"
+    content="Identify creators in the gaming space who are on the rise, and give them a launch pad to stardom.">
+  <meta property="og:site_name" content="Queued Up">
+  <meta property="og:url" content="<?php echo home_url( $wp->request ); ?>">
+  <meta property="og:description"
+    content="The selected creators will receive benefits and prize packages designed to help scale their brands, including a 12-month paid contract with HyperX and an all-expense paid trip to Los Angeles, CA.">
+  <meta property="og:type" content="website">
+  <link href="<?php echo get_template_directory_uri(); ?>/images/favicon/Icon_72x.webp" rel="apple-touch-icon"
+    sizes="72x72">
+  <link href="<?php echo get_template_directory_uri(); ?>/images/favicon/Icon_144x.webp" rel="apple-touch-icon"
+    sizes="144x144">
+  <link href="<?php echo get_template_directory_uri(); ?>/images/favicon/Icon_196x.webp" sizes="196x196" rel="icon"
+    type="image/webp">
+  <link href="<?php echo get_template_directory_uri(); ?>/images/favicon/Icon_96x.webp" sizes="96x96" rel="icon"
+    type="image/webp">
+  <link href="<?php echo get_template_directory_uri(); ?>/images/favicon/Icon_32x.webp" sizes="32x32" rel="icon"
+    type="image/webp">
+  <link href="<?php echo get_template_directory_uri(); ?>/images/favicon/Icon_16x.webp" sizes="16x16" rel="icon"
+    type="image/webp">
   <?php wp_head(); ?>
 </head>
 
