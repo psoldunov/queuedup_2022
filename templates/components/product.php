1b06b402ce242f13869a207345af1e8a8d07dd24
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2022-09-13 19:43:14 +0300

    product buttons

--- templates/components/product.php
+++ templates/components/product.php
@@ -1,19 +1,11 @@
 <div class="experience_product-instance">
   <div class="experience_product_thumb-wrap">
-    <div
-      class="experience_product_thumb_border is-border-1"
-    ></div>
-    <div 
-      class="experience_product_thumb_border is-border-2"
-    ></div>
-          <div class="experience_product_thumb_inner">
-        <img
-          src="<?php echo get_template_directory_uri(); ?>/images/<?php echo $args['image']; ?>"
-          loading="lazy"
-          alt="Photo of <?php echo $args['name']; ?>"
-          class="experience_product_thumb_image"
-        />
-      </div>
+    <div class="experience_product_thumb_border is-border-1"></div>
+    <div class="experience_product_thumb_border is-border-2"></div>
+    <div class="experience_product_thumb_inner">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/<?php echo $args['image']; ?>" loading="lazy"
+        alt="Photo of <?php echo $args['name']; ?>" class="experience_product_thumb_image" />
+    </div>
   </div>
   <div class="experience_product_info-block">
     <div class="experience_product_info_top-row">
@@ -24,6 +16,10 @@
         <?php echo $args['description']; ?>
       </div>
     </div>
-    <a href="<?php echo $args['link']; ?>" class="experience_product_cta-btn">Learn More</a>
+    <div class="product_button-grid">
+      <a href="<?php echo $args['learn_link']; ?>" target="_blank" class="button is-small is-product w-button">Learn
+        More</a>
+      <a href="<?php echo $args['buy_link']; ?>" target="_blank" class="button is-small is-product w-button">Buy Now</a>
+    </div>
   </div>
 </div>
--- templates/sections/experience-home.php
+++ templates/sections/experience-home.php
@@ -1,55 +1,55 @@
 <section class="section-experience wf-section">
-	<div class="section_relative-block">
-		<div class="section_content-layer">
-			<div class="page-padding">
-				<div class="container-regular">
-					<div class="padding-section-medium">
-						<div class="margin-bottom margin-huge">
-							<h2
-								class="heading-style-h2 text-font-rbno31 text-weight-black line-height-100 is-small-mobile"
-							>
-								<span class="text-style-nowrap-all">UPGRADE YOUR</span
-								><br /><span class="experience_h2-line text-color-pink"
-									>EXPERIENCE</span
-								>
-							</h2>
-						</div>
-						<div class="experience_product-grid">
+  <div class="section_relative-block">
+    <div class="section_content-layer">
+      <div class="page-padding">
+        <div class="container-regular">
+          <div class="padding-section-medium">
+            <div class="margin-bottom margin-huge">
+              <h2 class="heading-style-h2 text-font-rbno31 text-weight-black line-height-100 is-small-mobile">
+                <span class="text-style-nowrap-all">UPGRADE YOUR</span><br /><span
+                  class="experience_h2-line text-color-pink">EXPERIENCE</span>
+              </h2>
+            </div>
+            <div class="experience_product-grid">
               <?php get_component('product', [
                 'name'	=> 'HyperX CloudX Stinger Core Wireless Gaming Headset',
                 'image'	=> 'headset.webp',
                 'description' => 'Complete Wireless Freedom for Xbox',
-                'link' => '#',
+                'learn_link' => 'https://hyperx.com/products/hyperx-cloud-ii?variant=40998813827229',
+								'buy_link' => 'https://hyperx-us.myshopify.com/cart/40998813827229:1?attributes[source]=queuedup',
               ]) ?>
               <?php get_component('product', [
                 'name'	=> 'HyperX QuadCast',
                 'image'	=> 'mic.webp',
                 'description' => 'Full-featured standalone mic for streamers, content creators, and gamers.',
-                'link' => '#',
+                'learn_link' => '#',
+								'buy_link' => '#',
               ]) ?>
               <?php get_component('product', [
                 'name'	=> 'Alloy Origins 60',
                 'image'	=> 'keyboard.webp',
                 'description' => 'Make room for more plays.',
-                'link' => '#',
+                'learn_link' => '#',
+								'buy_link' => '#',
               ]) ?>
               <?php get_component('product', [
                 'name'	=> 'HyperX SoloCast',
                 'image'	=> 'solocast.webp',
                 'description' => 'A must have for video editors, streamers, and gamers',
-                'link' => '#',
+                'learn_link' => '#',
+								'buy_link' => '#',
               ]) ?>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="section_background-layer">
-			<div class="section_background-flex">
-				<div class="screen-container is-section-bg-container">
-					<div class="section_background-canvas"></div>
-				</div>
-			</div>
-		</div>
-	</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section_background-layer">
+      <div class="section_background-flex">
+        <div class="screen-container is-section-bg-container">
+          <div class="section_background-canvas"></div>
+        </div>
+      </div>
+    </div>
+  </div>
 </section>
